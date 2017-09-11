6e5eafd4af256de9499b766fc8be503c1059adee
Alan H.L. Li <hwaliangli@gmail.com>
2017-09-10 23:51:51 -1000

    EE618 HW2 alpha beta correcting

--- EE618/binary_programming.py
+++ EE618/binary_programming.py
@@ -8,6 +8,8 @@ M = 9
 def calculate(V, q):
     alphas = []
     betas = []
+    ymin = 0
+    ymax = 0
     for j in range(0, M, 1):
         ymin = np.log(V[j] * (np.prod(q[:, j])))
         ymax = np.log(V[j])
@@ -17,12 +19,13 @@ def calculate(V, q):
             [(1.) * np.power(ymax, 2) - np.power(ymin, 2), 2. * (ymax - ymin)]
         ]
 
-        B = [[2 * (np.exp(ymax) * (ymax - 1.) - np.exp(ymin) * (ymin - 1.))], [2 * (np.exp(ymax) - np.exp(ymin))]]
+        B = [2 * (np.exp(ymax) * (ymax - 1.) - np.exp(ymin) * (ymin - 1.)), 2 * (np.exp(ymax) - np.exp(ymin))]
 
-        answer = np.linalg.inv(A) * B
+        # answer = np.linalg.inv(A) * B
+        answer = np.linalg.solve(np.array(A), np.array(B))
         alphas.append(answer[0])
         betas.append(answer[1])
-    return alphas, betas
+    return ymin, ymax, alphas, betas
 
 
 def main():
@@ -30,17 +33,10 @@ def main():
     p = abs(np.sin(np.tile(np.arange(1.,1.+N)[:, np.newaxis], (1, M))) * np.sin(np.tile(np.arange(1., 1.+M), (N, 1))))
     q = 1 - p
 
-    alphas, betas = calculate(V, q)
+    ymin, ymax, alphas, betas = calculate(V, q)
 
-    for alphas in alphas:
-        print alphas
-    for beta in betas:
-        print beta
-
-    y = np.arange(-13.1, 2.3, 0.01)
-    plt.plot(y, np.exp(y))
-    plt.show()
-    plt.plot(y, np.array(alphas) * (y) + np.array(betas))
+    y = np.arange(ymin, ymax, 0.01)
+    plt.plot(y, np.exp(y), y, np.array(alphas[M-1]) * (y) + np.array(betas[M-1]))
     plt.show()
 
 
